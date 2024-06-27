a53192a60a1f23573c8f8efda2bab33b5802f9b5
CousemaAnjary <anjaryabdillah@gmail.com>
2024-06-27 23:45:25 +0300

    chore: Add Radix UI dependencies and update home page layout

---
+++ client/src/components/NavbarHome.tsx
@@ -0,0 +1,50 @@
+import logo from '../assets/images/logo.png'
+import { Button } from './ui/button'
+import { House, BadgeAlert, Contact, Settings, User } from 'lucide-react';
+import { DropdownMenu, DropdownMenuTrigger } from './ui/dropdown-menu';
+
+export default function NavbarHome() {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+
+
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+
+
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <>
+            <div className="container-fluid bg-white p-4 shadow">
+                <div className="mx-auto w-full max-w-6xl">
+                    <div className="hidden justify-between lg:flex">
+                        <div className="flex items-center gap-2">
+                            <img src={logo} alt="logo" className="w-24 h-auto object-contain" />
+
+                            <nav>
+                                <ul className="flex items-center gap-1">
+                                    <li><a href=""><Button variant="ghost"><House size={13} className="mr-1" /> Accueil</Button></a></li>
+                                    <li><a href=""><Button variant="ghost"><BadgeAlert size={13} className="mr-1" />A propos</Button></a></li>
+                                    <li><a href=""><Button variant="ghost"><Contact size={13} className="mr-1" />Contact</Button> </a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <Button variant="outline" className="p-2 shadow-sm"><Settings size={18} /></Button>
+                                </DropdownMenuTrigger>
+                            </DropdownMenu>
+                       
+                            <Button variant="ghost"className='font-serif'><User size={13} className='mr-1' />Connexion</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,6 @@
-import { Button } from "../components/ui/button";
+import Navbar from "../components/NavbarHome"
+import logo from '../assets/images/logo.png'
+import { Button } from "../components/ui/button"
 
 export default function Home() {
     /**
@@ -15,9 +17,17 @@ export default function Home() {
      * ! AFFICHAGE (render) de l'application
      */
     return (
-        <>
-            <h1>Bienvenue sur la page d'accueil </h1>
-            <Button>Click me</Button>
-        </>
+
+        <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+            <Navbar />
+            <div className="flex flex-col items-center justify-centertext-center mt-52 text-center">
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl font-serif">Découvrez de nouveaux amis avec<span><img src={logo} alt="logo" className="w-64 h-auto object-contain inline-block" /></span></h1>
+                <p className=" text-xl text-muted-foreground max-w-3xl"> Rejoignez notre communauté pour rencontrer de nouvelles personnes, partager des moments et explorer des intérêts communs.  </p>
+                <div className="mt-6 flex justify-center gap-4">
+                    <Button className="bg-cyan-700">Accèdez maintenant</Button>
+                    <Button variant="outline">Créer un compte</Button>
+                </div>
+            </div>
+        </div>
     )
 }
