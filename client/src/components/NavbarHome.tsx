8d0f3358e78ea621183150f76c0470fb50527317
CousemaAnjary <anjaryabdillah@gmail.com>
2024-06-28 18:06:22 +0300

    chore: Update home page layout and styles

--- client/src/components/NavbarHome.tsx
+++ client/src/components/NavbarHome.tsx
@@ -1,6 +1,6 @@
 import { Button } from './ui/button'
 import logo from '../assets/images/logo.png'
-import { DropdownMenu, DropdownMenuTrigger } from './ui/dropdown-menu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 import { House, BadgeAlert, Contact, Settings, User } from 'lucide-react'
 
 
@@ -19,33 +19,61 @@ export default function NavbarHome() {
      * ! AFFICHAGE (render) de l'application
      */
     return (
-        <>
-            <div className="container-fluid bg-white p-4 shadow">
-                <div className="mx-auto w-full max-w-6xl">
-                    <div className="hidden justify-between lg:flex">
-                        <div className="flex items-center gap-2">
-                            <img src={logo} alt="logo" className="w-24 h-auto object-contain" />
-
-                            <nav>
-                                <ul className="flex items-center gap-1">
-                                    <li><a href=""><Button variant="ghost"><House size={13} className="mr-1" /> Accueil</Button></a></li>
-                                    <li><a href=""><Button variant="ghost"><BadgeAlert size={13} className="mr-1" />A propos</Button></a></li>
-                                    <li><a href=""><Button variant="ghost"><Contact size={13} className="mr-1" />Contact</Button> </a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <DropdownMenu>
-                                <DropdownMenuTrigger>
-                                    <Button variant="outline" className="p-2 shadow-sm"><Settings size={18} /></Button>
-                                </DropdownMenuTrigger>
-                            </DropdownMenu>
-                       
-                            <Button variant="ghost"><User size={13} className='mr-1' />Connexion</Button>
-                        </div>
-                    </div>
+        <div className="container-fluid bg-white p-4 shadow">
+        <div className="mx-auto w-full max-w-6xl">
+            <div className="flex justify-between items-center">
+                <div className="flex items-center gap-4">
+                    <img src={logo} alt="logo" className="w-24 h-auto object-contain" />
+                    <nav>
+                        <ul className="flex items-center gap-4">
+                            <li>
+                                <a href="">
+                                    <Button variant="ghost" className="flex items-center gap-1">
+                                        <House size={18} className="mr-1" /> Accueil
+                                    </Button>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <Button variant="ghost" className="flex items-center gap-1">
+                                        <BadgeAlert size={18} className="mr-1" /> A propos
+                                    </Button>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <Button variant="ghost" className="flex items-center gap-1">
+                                        <Contact size={18} className="mr-1" /> Contact
+                                    </Button>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="flex items-center gap-4">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" className="p-2 shadow-sm">
+                                <Settings size={18} />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>Theme</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuGroup>
+                                <DropdownMenuItem>Light</DropdownMenuItem>
+                                <DropdownMenuItem>Dark</DropdownMenuItem>
+                            </DropdownMenuGroup>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <a href="">
+                        <Button variant="ghost" className="flex items-center gap-1">
+                            <User size={18} className="mr-1" /> Connexion
+                        </Button>
+                    </a>
                 </div>
             </div>
-        </>
+        </div>
+    </div>
     )
 }
