38c21336bd544c933d1f9fe34b719423663187be
CousemaAnjary <anjaryabdillah@gmail.com>
2024-06-30 17:22:28 +0300

    chore: Update DashboardClient.tsx with optimized styling and background gradient

--- client/src/pages/DashboardClient.tsx
+++ client/src/pages/DashboardClient.tsx
@@ -18,7 +18,25 @@ export default function DashboardClient() {
      * ! AFFICHAGE (render) de l'application
      */
     return (
-        <div className="bg-gray-200 min-h-screen flex flex-col">
+        // <div className="bg-gray-200 min-h-screen flex flex-col">
+        //     <Navbar />
+        //     <div className="flex flex-1 mt-10 h-full">
+        //         {/* <div className="flex-shrink-0 w-64 mr-4">
+        //             <Sidebar />
+        //         </div> */}
+
+        //         <main className="flex-grow bg-slate-300 mx-4">
+        //             {/* Contenu principal */}
+        //         </main>
+
+        //         <div className="flex-shrink-0 w-64 ml-4">
+        //             <SidebarRight />
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div className="relative min-h-screen w-full ">
+           <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,160,255,0.08)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
             <Navbar />
             <div className="flex flex-1 mt-10 h-full">
                 {/* <div className="flex-shrink-0 w-64 mr-4">
@@ -27,6 +45,7 @@ export default function DashboardClient() {
 
                 <main className="flex-grow bg-slate-300 mx-4">
                     {/* Contenu principal */}
+                    
                 </main>
 
                 <div className="flex-shrink-0 w-64 ml-4">
