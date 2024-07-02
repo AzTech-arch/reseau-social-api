6deb55eb486a9c70efaffc82a6d86a5b314047eb
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-02 14:37:52 +0300

    chore: Update ChatClient page layout and remove unused components

--- client/src/pages/ChatClient.tsx
+++ client/src/pages/ChatClient.tsx
@@ -1,33 +1,20 @@
 import ChatLayout from "../components/ChatLayout";
 import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
-import SidebarChat from "../components/SidebarChat";
 import SidebarRight from "../components/SidebarRight";
-import { useState } from "react";
+
 
 export default function Chat() {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
-
 
     return (
         <div className="relative min-h-screen w-full">
             {/* Background Gradient */}
             <div className="absolute bg-fixed inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
-
             <Navbar />
 
             <div className="flex flex-1 mt-10 h-full overflow-x-hidden">
-                {/* Left Sidebar */}
-                <div className={`transition-all duration-700 ${isSidebarOpen ? 'mr-20' : 'mr-20'}`}>
-                    <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-                </div>
 
                 {/* Main Content */}
-                <main className={`flex-grow  transition-all duration-300 ${isSidebarOpen ? 'ml-64' : 'ml-20'}`}>
+                <main className="flex-grow  transition-all duration-300 ml-20">
                     {/* Contenu principal */}
                     <ChatLayout />
                 </main>
