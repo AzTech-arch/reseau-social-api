6c88ad56d4c7074b353b0c14bc6c36842ee01237
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-03 19:00:17 +0300

    register

---
+++ client/src/services/authService.ts
@@ -0,0 +1,17 @@
+import api from './apiConfig'
+
+// Définir les interfaces des données de l'utilisateur
+interface dataRegisterProps { lastname: string, firstname: string, email: string, password: string }
+
+
+// Inscrire un nouvel utilisateur
+export const register = async (dataRegister: dataRegisterProps) => {
+    try {
+        // Appel à l'API pour enregistrer un nouvel utilisateur
+        const response = await api.post('/register', dataRegister)
+        return response.data; // Retourner les données de la réponse de l'API
+
+    } catch (error) {
+        console.error('Erreur lors de l\'inscription:', error);
+    }
+}
