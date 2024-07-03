0d8ea918e96adaa4ab5586a079a22ab0abdd2e6b
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-03 18:41:31 +0300

    chore: Add registration API endpoint for guest users

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::middleware('guest')->group(function () {
+
+    Route::controller(RegisterController::class)->group(function () {
+        Route::post('register', 'store');
+    });
+});
+
+Route::middleware('auth')->group(function () {
 });
