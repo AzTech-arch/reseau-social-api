b8605934c3e728b8163d4ae61dcfa5d02b40388b
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-03 17:59:16 +0300

    chore: Update User model and migration with additional fields

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'lastname',
+        'firstname',
         'email',
         'password',
+        'image',
+        'image_cover',
     ];
 
     /**
