7ca2542e520979bf2f5eae9827fe0317ace29747
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-04 18:16:28 +0300

    chore: Add login functionality to authService

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -2,6 +2,7 @@ import api from './apiConfig'
 
 // Définir les types des données du formulaire
 type dataRegisterProps = { last_name: string, first_name: string, email: string, password: string }
+type dataLoginProps = { email: string, password: string };
 
 
 // Inscrire un nouvel utilisateur
@@ -9,10 +10,23 @@ export const register = async (dataRegister: dataRegisterProps) => {
     try {
         // Appel à l'API pour enregistrer un nouvel utilisateur
         const response = await api.post('/register', dataRegister)
-        return response.data; // Retourner les données de la réponse de l'API
+        return response.data // Retourner les données de la réponse de l'API
 
     } catch (error) {
         // Gérer les erreurs et les afficher dans la console
         console.error('Erreur lors de l\'inscription:', error)
     }
 }
+
+// Connecter un utilisateur
+export const login = async (dataLogin: dataLoginProps) => {
+    try {
+        // Appel à l'API pour connecter un utilisateur
+        const response = await api.post('/login', dataLogin);
+        return response.data // Retourner les données de la réponse de l'API
+
+    } catch (error) {
+        // Gérer les erreurs et les afficher dans la console
+        console.error('Erreur lors de la connexion:', error);
+    }
+};
