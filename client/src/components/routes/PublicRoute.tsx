65f0a413e0e17d192e2757739b08d00eb56cbf93
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-06 17:08:56 +0300

    chore: Supprimer les fichiers et configurations inutilisés

--- client/src/components/routes/PublicRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { isAuthenticated } from '../../utils/auth';
-
-const PublicRoute = () => {
-  return isAuthenticated() ? <Navigate to="/friendzy" /> : <Outlet />;
-};
-
-export default PublicRoute;
