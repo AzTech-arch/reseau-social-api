067d07d6c9731a2e2807405d6a3909a037b34085
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-07 22:05:47 +0300

    chore: Mettre à jour les dépendances pour utiliser Laravel Echo et Pusher.js

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Frontend\FriendController;
 use App\Http\Controllers\Api\Frontend\DashboardController;
 use App\Http\Controllers\Api\Auth\AuthenticatedUserSessionController;
 
@@ -22,9 +23,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
+    // Auth routes
     Route::post('logout', [AuthenticatedUserSessionController::class, 'destroy']);
+    // Dashboard routes
     Route::post('updateUserImage', [DashboardController::class, 'updateUserImage']);
     Route::post('updateCoverImage', [DashboardController::class, 'updateCoverImage']);
+    // Friend routes
+    Route::post('friend/request', [FriendController::class, 'sendRequest']);
+    Route::post('friend/accept/{id}', [FriendController::class, 'acceptRequest']);
+    Route::post('friend/reject/{id}', [FriendController::class, 'rejectRequest']);
+    Route::delete('friend/remove/{id}', [FriendController::class, 'removeFriend']);
 });
 
 // Route public 
